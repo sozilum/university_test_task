826b38bc64b5b9e69ba908b25022943ab37963e1
verghil_mac <verghil_mac>
2026-06-08 20:24:19 +0300

    changes description, essense of visioner. dragon begin

---
+++ dragon/src/main.rs
@@ -0,0 +1,21 @@
+
+pub fn dragon(num: i32){
+
+    
+
+
+}
+
+
+
+fn main() {
+    println!("Введите число для расчёта максимальной мощности: {}", dragon);
+    
+    let mut aux_num: String = String::new();
+
+    io::stdin().read_line(&mut aux_num).expect();
+    
+    let total = dragon(aux_num as i32);
+    
+
+}
